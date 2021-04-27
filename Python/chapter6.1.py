ecacc3832a6273700c1e779331e25e1b76ae2547
emelie495 <lisa.litsche17@gmail.com>
2021-04-27 08:44:04 +0200

    LOGIST-100

--- Python/chapter6.1.py
+++ Python/chapter6.1.py
@@ -9,7 +9,7 @@ RED = (255, 0, 0)
 pygame.init()
  
 # Set the width and height of the screen [width, height]
-size = (700, 500)
+size = (300, 200)
 screen = pygame.display.set_mode(size)
  
 pygame.display.set_caption("My Game")
@@ -39,25 +39,109 @@ while not done:
     screen.fill(BLACK)
  
     # --- Drawing code should go here
-    ay=0
-    for ay in range (20):
-        ay=ay+10
-    for y_offset in range(0, 500, 10):
-        pygame.draw.rect(screen, GREEN, [ay+y_offset, 0+y_offset, 5, 5])
-        for y_offset in range(0, 500, 10):
-            pygame.draw.rect(screen, GREEN, [10+y_offset, 0+y_offset, 5, 5])
-        for y_offset in range(0, 600, 10):
-            pygame.draw.rect(screen, GREEN, [20+y_offset, 0+y_offset, 5, 5])
-        for y_offset in range(0, 600, 10):
-            pygame.draw.rect(screen, GREEN, [30+y_offset, 0+y_offset, 5, 5])
-        for y_offset in range(0, 600, 10):
-            pygame.draw.rect(screen, GREEN, [40+y_offset, 0+y_offset, 5, 5])
-        for y_offset in range(0, 600, 10):
-            pygame.draw.rect(screen, GREEN, [50+y_offset, 0+y_offset, 5, 5])
-        for y_offset in range(0, 600, 10):
-            pygame.draw.rect(screen, GREEN, [60+y_offset, 0+y_offset, 5, 5])
-        for y_offset in range(0, 600, 10):
-            pygame.draw.rect(screen, GREEN, [70+y_offset, 0+y_offset, 5, 5])
+    #ay=0
+    #for ay in range (20):
+       # ay=ay+10
+    for y_offset in range(0, 400, 10):
+         pygame.draw.rect(screen, GREEN, [0+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [10+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [20+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [30+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [40+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [50+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [60+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [70+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [80+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [90+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [100+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [110+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [120+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [130+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [140+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [150+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [160+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [170+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [180+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [190+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [200+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [210+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [220+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [230+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [240+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [250+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [260+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [270+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [280+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [290+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [300+y_offset, 0+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 10+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 20+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 30+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 40+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 50+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 60+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 70+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 80+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 90+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 100+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 110+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 120+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 130+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 140+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 150+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 160+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 170+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 180+y_offset, 5, 5])
+    for y_offset in range(0, 400, 10):
+        pygame.draw.rect(screen, GREEN, [0+y_offset, 190+y_offset, 5, 5])
  
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
